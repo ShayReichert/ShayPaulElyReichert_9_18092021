6dd63bce76092f5bb4af2b0432c0833b91e3a031
Shay Reichert <shay.reichert@outlook.fr>
2021-11-01 12:55:57 +0100

    Clean

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -25,7 +25,6 @@ export default class {
     const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
     $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`)
     if (typeof $("#modaleFile").modal === "function") $("#modaleFile").modal("show");
-    // $('#modaleFile').modal('show')
   }
 
   // not need to cover this function by tests
